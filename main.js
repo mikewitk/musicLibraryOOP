79739310d08eab6c3dc034f7d5fd6465e9e05cc9
Michael <mike.witk@gmail.com>
2019-05-11 18:15:19 +0000

    totalDuration

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ Playlist.prototype.addTracks = function addTracks( track ) {
   this.tracks.push( track );
 }
 
-Playlist.prototype.overallRating = function overallRating( ) {
+Playlist.prototype.overallRating = function overallRating() {
   
   let sum = 0;
   let average = 0;
@@ -43,7 +43,21 @@ Playlist.prototype.overallRating = function overallRating( ) {
     });
   
   average = sum / this.tracks.length;
-  return "The average rating is: " + average
+  return ("The playlist '" + this.name + "' have an average rating of '" + average + "'")
+}
+
+Playlist.prototype.totalDuration = function totalDuration() {
+
+  let sum = 0;
+
+  this.tracks.forEach( (track) => {
+    sum+= track.length
+  });
+
+  let minutes = Math.floor(sum / 60);
+  let seconds = sum - minutes * 60;
+
+  return ("The playlist '" + this.name + "' have a total duration of " + minutes + "min " + seconds +  "s.")
 }
 
 function Track( title, rating, length ) {
@@ -55,16 +69,16 @@ function Track( title, rating, length ) {
 
 // Tests
 
-let newLibrary = new Library( "My Library", "Mike Witk" );
+let newLibrary = new Library( "Songs for You", "Mike Witk" );
 // console.log("This is my new library: ", newLibrary );
 
-let playlist1 = new Playlist( "My Playlist" );
-let playlist2 = new Playlist( "My second playlist" );
-let playlist3 = new Playlist( "My third playlist" );
+let playlist1 = new Playlist( "To Code" );
+let playlist2 = new Playlist( "To Chase Dreams" );
+let playlist3 = new Playlist( "Chill AF" );
 
 newLibrary.addPlaylist( playlist1 );
-// newLibrary.addPlaylist( playlist2 );
-// newLibrary.addPlaylist( playlist3 );
+newLibrary.addPlaylist( playlist2 );
+newLibrary.addPlaylist( playlist3 );
 
 // console.log( "This is my library with new playlist: ", newLibrary);
 
@@ -77,6 +91,31 @@ let track5 = new Track ( "Second Wind", 5, 135 );
 playlist1.addTracks( track1 );
 playlist1.addTracks( track3 );
 playlist1.addTracks( track4 );
-console.log( "These are the tracks on my playlist1: ", playlist1 );
+// console.log( "These are the tracks on my playlist1: ", playlist1 );
 
-console.log( playlist1.overallRating() );
+playlist2.addTracks( track2 )
+playlist2.addTracks( track4 )
+playlist2.addTracks( track5 )
+
+playlist3.addTracks( track1 )
+playlist3.addTracks( track2 )
+playlist3.addTracks( track3 )
+playlist3.addTracks( track4 )
+playlist3.addTracks( track5 )
+
+
+playlist1.overallRating();
+playlist1.totalDuration();
+playlist2.overallRating();
+playlist2.totalDuration();
+playlist3.overallRating();
+playlist3.totalDuration();
+
+console.log( "This is my new library: ", newLibrary );
+
+console.log( playlist1.overallRating() )
+console.log( playlist1.totalDuration() )
+console.log( playlist2.overallRating() )
+console.log( playlist2.totalDuration() )
+console.log( playlist3.overallRating() )
+console.log( playlist3.totalDuration() )
